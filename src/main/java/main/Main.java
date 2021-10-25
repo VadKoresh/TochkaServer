c2d8f4169bf54c61a5a8e2903ad8d0a63d1dc154
Vadim Koreshkov <evilgod-2@mail.ru>
2021-10-25 05:26:10 +0300

    add GetUpApp

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import main.service.GetUpApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        GetUpApp getUpApp = new GetUpApp();
+        getUpApp.getUp();
     }
 }
---
+++ src/main/java/main/service/GetUpApp.java
@@ -0,0 +1,41 @@
+package main.service;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
+public class GetUpApp {
+
+    public void getUp() {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                CloseableHttpClient httpClient = null;
+                CloseableHttpResponse httpResponse = null;
+                try {
+                    Thread.sleep(1800000);
+                    httpClient = HttpClients.createDefault();
+                    HttpGet httpGet = new HttpGet("https://tochkaserver.herokuapp.com/admin/?login=admin&password=admin1");
+                    httpResponse = httpClient.execute(httpGet);
+                    HttpEntity httpEntity = httpResponse.getEntity();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        httpResponse.close();
+                        httpClient.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+    }
+}
